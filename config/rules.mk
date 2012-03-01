376368b7c82d5b972f33426dda9a673b238a4996
Mike Hommey <mh@glandium.org>
2012-03-01 10:38:16 +0100

    Support building build tools (szip)

--- config/rules.mk
+++ config/rules.mk
@@ -1,3 +1,5 @@
+VPATH := $(foreach path,$(VPATH),$(path:%/mfbt=%/mozilla))
+
 ifdef LIBRARY_NAME
 LIBRARY = lib$(LIBRARY_NAME).a
 TARGETS = $(LIBRARY)
@@ -5,6 +7,9 @@ else
 ifdef SIMPLE_PROGRAMS
 TARGETS = $(SIMPLE_PROGRAMS)
 endif
+endif
+ifdef HOST_PROGRAM
+TARGETS += $(HOST_PROGRAM)
 endif
 
 OBJS = $(CPPSRCS:.cpp=.o)
@@ -24,3 +29,13 @@ $(LIBRARY): $(OBJS)
 
 $(OBJS): %.o: %.cpp
 	$(CXX) $(CXXFLAGS) -include $(DEPTH)/config.h $(LOCAL_INCLUDES) -I$(topsrcdir) -o $@ -c $<
+
+# HOST doesn't mean the same as in configure.ac. Here, it means "build".
+# This is due to the makefiles coming from a system using configure 2.13.
+HOSTOBJS = $(HOST_CPPSRCS:%.cpp=host_%.o)
+
+$(HOST_PROGRAM): $(HOSTOBJS)
+	$(BUILDCXX) -o $@ $^ $(BUILDLDFLAGS) $(HOST_LIBS)
+
+$(HOSTOBJS): host_%.o: %.cpp
+	$(BUILDCXX) $(BUILDCXXFLAGS) -I$(topsrcdir) -o $@ -c $<
